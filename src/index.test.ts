b66fd3a5dfcb42c02a8ed8bec84ba479308313eb
jdtibbs <jdtibbs@users.noreply.github.com>
2019-01-12 18:24:50 -0600

    x

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,4 +1,4 @@
-import { add, fromMaybe, head } from 'sanctuary';
+import { add, compose, fromMaybe, head } from 'sanctuary';
 
 test('sanctuary add5 = 8', () => {
   const add5 = add(5);
@@ -8,3 +8,9 @@ test('sanctuary add5 = 8', () => {
 test('head = 3', ()=>{
   expect(fromMaybe(0)(head([3,4,5]))).toBe(3)
 }
+ test('compose', ()=>{
+  const add2 = add(2);
+  const add5 = add(5);
+  const add3 = add(3);
+  expect(compose(add2(1))(add5(1))(add3(1))(1)).toBe(10))
+})
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
-import { add } from 'sanctuary';
-const add5 = add(5)
-console.log(add5(10))
+import { add, compose, Just, Nothing, pipe } from 'sanctuary';
+
+const add5 = add(5);
+console.log(add5(10));
+
+const add2 = add(2);
+console.log(add2(1));
+const add5 = add(5);
+console.log(add5(5));
+const add3 = add(3);
+console.log(add3(3));
+const x = compose(Math.sqrt)(add(1))(99);
+console.log(x);
+console.log(pipe ([add(5) add(4) add(3) add(2)]) (1))
+console.log(Just)
+console.log(Nothing)
