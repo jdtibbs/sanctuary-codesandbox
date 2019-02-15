b407f86e4d88a014ced501b93d8af9affe0071ed
jdtibbs <jdtibbs@users.noreply.github.com>
2019-02-15 16:19:25 -0600

    object

--- src/maybe.test.js
+++ src/maybe.test.js
@@ -2,7 +2,6 @@ import {
   add,
   even,
   fromMaybe,
-  get,
   ifElse,
   isJust,
   isNothing,
@@ -10,9 +9,7 @@ import {
   Just,
   Nothing,
   odd,
-  pipe,
-  prop,
-  show
+  pipe
 } from 'sanctuary';
 
 /**
@@ -27,27 +24,20 @@ Nothing = false / empty value
 */
 
 test('isJust / isNothing', () => {
-  const name = Just('fred');
-  expect(isJust(name)).toBe(true);
-  const nothing = Nothing;
-  expect(isNothing(nothing)).toBe(true);
+  expect(isJust(Just('fred'))).toBe(true);
+  expect(isNothing(Nothing)).toBe(true);
 });
 
-test('fromMaybe -> default or value', () => {
-  const name = Just('fred');
-  expect(fromMaybe('')(name)).toBe('fred');
-  const nothing = Nothing;
-  expect(fromMaybe('nothing')(nothing)).toBe('nothing');
+test('fromMaybe => default or value', () => {
+  expect(fromMaybe('')(Just('fred'))).toBe('fred');
+  expect(fromMaybe('nothing')(Nothing)).toBe('nothing');
 });
 
-const produce = { id: 1, name: 'apple' };
-const getName = prop('name');
 const addOne = pred => ifElse(pred)(add(1))(I);
 const _addTwo = pipe([add(2), Just]);
-// const addTwo = pred => ifElse(pred)(_addTwo)(Just);
 const addTwo = pred => ifElse(pred)(pipe([add(2), Just]))(Just);
 
-test('fred', () => {
+test('addOne / addTwo', () => {
   expect(addOne(even)(1)).toBe(1);
   expect(addOne(even)(2)).toBe(3);
   expect(fromMaybe(0)(_addTwo(1))).toBe(3);
---
+++ src/object.test.js
@@ -0,0 +1,47 @@
+import {
+  encase,
+  equals,
+  fromMaybe,
+  get,
+  gets,
+  maybe,
+  prop,
+  props,
+  pipe
+} from 'sanctuary';
+
+const log = l => v => {
+  console.log(l, v);
+  return v;
+};
+
+const _prop = pipe([prop, encase]);
+const _props = pipe([props, encase]);
+
+test('prop', () => {
+  const o = { id: 1, name: 'fred' };
+  expect(fromMaybe(0)(pipe([prop, encase])('id')(o))).toBe(1);
+  expect(fromMaybe(0)(_prop('id')(o))).toBe(1);
+  expect(fromMaybe('')(_prop('name')(o))).toBe('fred');
+  expect(fromMaybe(0)(_prop('age')(o))).toBe(0);
+});
+
+test('props', () => {
+  const o = { id: 1, name: 'fred', pets: { dog: true, cat: false } };
+  const hasDog = _props(['pets', 'dog']);
+  const hasCat = _props(['pets', 'cat']);
+  expect(fromMaybe(false)(hasDog(o))).toBe(true);
+  expect(fromMaybe(false)(hasCat(o))).toBe(false);
+});
+
+test('get', () => {
+  const o = { id: 1, name: 'sam' };
+  expect(fromMaybe(0)(get(equals(1))('id')(o))).toBe(1);
+  expect(fromMaybe(0)(get(equals(2))('id')(o))).toBe(0);
+});
+
+test('gets', () => {
+  const o = { id: 1, name: 'sam', pets: { dog: true, cat: false } };
+  expect(fromMaybe(true)(gets(equals(true))(['pets', 'dog'])(o))).toBe(true);
+  expect(fromMaybe(false)(gets(equals(false))(['pets', 'cat'])(o))).toBe(false);
+});
